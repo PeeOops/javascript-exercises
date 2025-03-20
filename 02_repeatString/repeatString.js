9c6d709e115be3cbf27bf072af285186563fc71e
PeeOops <51711535+PeeOops@users.noreply.github.com>
2025-03-20 23:37:42 +0700

    Done 8 Exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let temp = [];
+    for(let i = 0; i < num; i++){
+        temp.push(string);
+    }
+    if (num < 0){
+        return "ERROR";
+    }else{
+        return temp.join("");
+    }
 };
+
+// repeatString("hello", 0)
 
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let temp = []
+    if(num1 > num2){
+        let start = num2;
+        let end = num1;
+
+        if(start > 0 && end > 0 && Number.isInteger(start) && Number.isInteger(end)){
+            for(let i = start; i <= end; i++){
+                temp.push(i);
+            }
+    
+            const result = temp.reduce((acc,value) => acc + value);
+            return result;
+        }else{
+            return "ERROR";
+        }
+    }else{
+        if(num1 > 0 && num2 > 0 && Number.isInteger(num1) && Number.isInteger(num2)){
+            for(let i = num1; i <= num2; i++){
+                temp.push(i);
+            }
+    
+            const result = temp.reduce((acc,value) => acc + value);
+            return result;
+        }else{
+            return "ERROR";
+        }
+    }
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
